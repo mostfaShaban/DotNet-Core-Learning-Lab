aa117d52c0affedfb3cff4ff4efca2fb1bd4ba7c
mostfaShaban <110400597+mostfaShaban@users.noreply.github.com>
2026-06-02 15:27:35 +0300

    Squashed '01-OOP-Fundamentals/Day01-Solution/' content from commit 7c3aed1
    git-subtree-dir: 01-OOP-Fundamentals/Day01-Solution
    git-subtree-split: 7c3aed1efa6b90075669bed033574d189330837a

---
+++ Ass01-OOP/WeekDays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass01_OOP
+{
+    internal enum WeekDays
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 3,
+        Wednesday = 4,
+        Thursday = 5,
+        Friday = 6,
+        Saturday = 7
+    }
+    //Create an Enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members
+    internal enum Seas_on
+    {
+        Spring = 1,
+        Summer = 2,
+        Autumn = 3,
+        Winter = 4
+    }
+
+    internal enum Colors
+    {
+        Red,
+        Green,
+        Blue
+    }
+}
