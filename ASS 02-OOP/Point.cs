6e7655b7cde2309efa520e104cb41baf08f6cfc1
mostfaShaban <110400597+mostfaShaban@users.noreply.github.com>
2026-06-02 15:29:33 +0300

    Squashed '01-OOP-Fundamentals/Day02-Solution/' content from commit 63f9d70
    git-subtree-dir: 01-OOP-Fundamentals/Day02-Solution
    git-subtree-split: 63f9d70b907c5c9ad4f8e5d343cbb71f7724c4bf

---
+++ ASS 02-OOP/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS_02_OOP
+{
+    internal struct Point
+    {
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double CalculateDistance(Point secondPoint)
+        {
+            // القانون: √((x2-x1)² + (y2-y1)²)
+            return Math.Sqrt(Math.Pow(secondPoint.X - this.X, 2) + Math.Pow(secondPoint.Y - this.Y, 2));
+        }
+    }
+}
---
+++ ASS 02-OOP/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS_02_OOP
+{
+    internal struct Rectangle
+    {
+        private double width;
+        private double height;
+
+
+        public Double Width
+        {
+            get { return width; }
+
+            set
+            {
+                if (value > 0)
+                    width = value;
+                else
+                    Console.WriteLine("Error width can't negative");
+            }
+
+        }
+
+        public Double Height
+        {
+            get { return height; }
+            
+            set {
+                if (value > 0)
+                    height = value;
+                else
+                    Console.WriteLine("Error height can't negative");
+            }
+        }
+
+        public double Area
+        {
+            get { return width * height; }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Area: " + Area);
+        }
+
+    }
+}
